022c42253f9471a0feb2376bed553441f326c87e
hyunjungjung <hjung3@my.centennialcollege.ca>
2016-07-12 13:37:12 -0400

    Added mock login functionality to the login form

---
+++ COMP123_Lesson9/LoginForm.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMP123_Lesson9
+{
+    public partial class LoginForm : Form
+    {
+        /**
+         * <summary>
+         * This is the default constructor for the LoginForm Class
+         * </summary>
+         * @constructor LoginForm
+         */
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        // PRIVATE METHODS ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+        /**
+         * <summary>
+         * This method clears the UsernameTextBox and the PasswordTextBox
+         * </summary>
+         * 
+         * @private
+         * @method _clearForm
+         * @returns {void}
+         */ 
+        private void _clearForm()
+        {
+            UsernameTextBox.Clear();
+            PasswordTextBox.Clear();
+        }
+
+        /**
+         * <summary>
+         * This is the LoginButton Click event handler
+         * </summary>
+         * 
+         * @private
+         * @method LoginButton_Click
+         * @param {object} sender
+         * @param {EventArgs} e
+         * @return {void}
+         */
+        private void LoginButton_Click(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Username: " + UsernameTextBox.Text);
+            Debug.WriteLine("Password: " + PasswordTextBox.Text);
+            this._clearForm();
+        }
+    }
+}
